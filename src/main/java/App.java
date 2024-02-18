792c404be60bbb1d5c7796801b2867f199a20b6e
CaranMoldovan <116651970+CaranMoldovan@users.noreply.github.com>
2024-02-18 15:08:18 +0300

    First Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld =(HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat = (Cat)  applicationContext.getBean("Cat");
+        Cat secondCat= (Cat)    applicationContext.getBean("Cat");
+        System.out.println(helloWorld== bean);
+        System.out.println(firstCat==secondCat);
     }
 }
